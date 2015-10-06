2bca15e8f67eceb308060853f62739de2fc6692b
Karolis Astrauka <astrauka@gmail.com>
2015-10-06 18:08:23 +0300

    Add hasVoted

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,32 @@
-import {Map} from 'immutable';
+import {List, Map} from 'immutable';
+
+function currentPair(state) {
+  return List(state.getIn(['vote', 'pair']));
+}
+
+function vote(state, entry) {
+  if (currentPair(state).includes(entry)) {
+    return state.set('hasVoted', entry);
+  } else {
+    return state;
+  }
+}
+
+function resetHasVoted(state) {
+  const hasVoted = state.get('hasVoted');
+  if (currentPair(state).includes(hasVoted)) {
+    return state;
+  } else {
+    return state.remove('hasVoted');
+  }
+}
 
 export default function reducer(state = Map(), action) {
   switch (action.type) {
   case 'SET_STATE':
-    return state.merge(action.state);
+    return resetHasVoted(state.merge(action.state));
+  case 'VOTE':
+    return vote(state, action.entry);
   }
 
   return state;
--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -17,4 +17,47 @@ describe('reducer', () => {
     expect(reducer(undefined, action)).to.equal(receivedState);
     expect(reducer(initialState, action)).to.equal(receivedState);
   });
+
+  it('removes hasVoted on SET_STATE pair change', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+      },
+      hasVoted: 'Trainspotting',
+    });
+
+    const noPairChangeState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+      }
+    });
+    const noPairChangeAction = {type: 'SET_STATE', state: noPairChangeState};
+    expect(reducer(initialState, noPairChangeAction)).to.equal(initialState);
+
+    const pairChangeState = fromJS({
+      vote: {
+        pair: ['Sunshine', 'Millionaire']
+      }
+    });
+    const pairChangeAction = {type: 'SET_STATE', state: pairChangeState};
+    expect(reducer(initialState, pairChangeAction)).to.equal(pairChangeState);
+  });
+
+  it('handles VOTE by setting hasVoted', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 2}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    expect(reducer(initialState, action)).to.equal(
+      initialState.merge({
+        hasVoted: 'Trainspotting',
+      })
+    );
+
+    const invalidEntryAction = {type: 'VOTE', entry: 'Sunrize'};
+    expect(reducer(initialState, invalidEntryAction)).to.equal(initialState);
+  });
 });
